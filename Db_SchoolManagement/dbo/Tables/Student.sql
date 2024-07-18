333bd58d263f315ba2621f0ee47bbdf1a5716814
Victor Kumar Barua <victor.tts@hotmail.com>
2024-07-18 11:23:10 +0200

    Add project files.

---
+++ Db_SchoolManagement/dbo/Tables/Student.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Student] (
+    [Id]         INT           IDENTITY (1, 1) NOT NULL,
+    [AddedDate]  DATETIME      NULL,
+    [FirstName]  VARCHAR (50)  NULL,
+    [LastName]   VARCHAR (50)  NULL,
+    [Gender]     VARCHAR (10)  NULL,
+    [FatherName] VARCHAR (50)  NULL,
+    [MotherName] VARCHAR (50)  NULL,
+    [Address]    VARCHAR (500) NULL,
+    [City]       VARCHAR (50)  NULL,
+    [Country]    VARCHAR (50)  NULL,
+    [Phone]      BIGINT        NULL,
+    [Status]     CHAR (1)      NULL,
+    CONSTRAINT [PK_Student] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
